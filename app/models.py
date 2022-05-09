fcf9e74b20b9aec7529456e7d1a84e9af5ba51a3
Shalyne-Wangari <shalyne.waweru@student.moringaschool.com>
2022-05-09 16:46:12 +0300

    Create the User Model an the Flask-script shell

--- app/models.py
+++ app/models.py
@@ -1,3 +1,15 @@
+from . import db
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer,primary_key = True)
+    email = db.Column(db.String(255),unique = True,index = True)
+    username = db.Column(db.String(255),unique = True,index = True)
+    password = db.Column(db.String(255))
+
+    def __repr__(self):
+        return f'User {self.username}'
+
 class Pickup:
 
     all_pickup_lines = []
