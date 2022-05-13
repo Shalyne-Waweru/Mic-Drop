9211a5976dee98aea10826f004847dcf82c779c0
Shalyne-Wangari <shalyne.waweru@student.moringaschool.com>
2022-05-13 11:38:01 +0300

    Modify the config.py file

--- config.py
+++ config.py
@@ -26,7 +26,7 @@ class ProdConfig(Config):
         SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://",1)
 
 class TestConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://shalyne:12345@localhost/pitches_test'
+    SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://shalyne:12345@localhost/pitches_test"
 
 class DevConfig(Config):
     '''
@@ -35,7 +35,7 @@ class DevConfig(Config):
     Args:
         Config: The parent configuration class with General configuration settings
     '''
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://shalyne:12345@localhost/pitches'
+    SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://shalyne:12345@localhost/pitches"
 
     DEBUG = True
 
